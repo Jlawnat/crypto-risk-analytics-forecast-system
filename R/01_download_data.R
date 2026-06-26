73eff8977c57285342e1301ac79f254a653cc55c
Elliot Yang <ramsyhahaha@gmail.com>
2026-06-26 22:43:53 +1000

    raw data

--- R/01_download_data.R
+++ R/01_download_data.R
@@ -15,3 +15,36 @@ BNB <- Ad(getSymbols("BNB-USD",
                      src = "yahoo",
                      from = "2020-01-01",
                      auto.assign = FALSE))
+
+write_csv(
+  data.frame(
+    Date = index(BTC),
+    Price = coredata(BTC)
+  ),
+  "data/raw/BTC_prices.csv"
+)
+
+write_csv(
+  data.frame(
+    Date = index(ETH),
+    Price = coredata(ETH)
+  ),
+  "data/raw/ETH_prices.csv"
+)
+
+write_csv(
+  data.frame(
+    Date = index(SOL),
+    Price = coredata(SOL)
+  ),
+  "data/raw/SOL_prices.csv"
+)
+
+write_csv(
+  data.frame(
+    Date = index(BNB),
+    Price = coredata(BNB)
+  ),
+  "data/raw/BNB_prices.csv"
+)
+
