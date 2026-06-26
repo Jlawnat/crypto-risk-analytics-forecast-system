1f8531b0d61fef07194f293f734295cf683bf4ce
Elliot Yang <ramsyhahaha@gmail.com>
2026-06-26 20:11:55 +1000

    adf&arch effect test

--- R/03_modelling.R
+++ R/03_modelling.R
@@ -81,7 +81,63 @@ dev.off()
 
 par(mfrow=c(1,1))
 
+library(tseries)
+library(dplyr)
+adf_results <- data.frame(
+  Cryptocurrency = character(),
+  ADF_Statistic = numeric(),
+  P_Value = numeric(),
+  Stationary = character(),
+  stringsAsFactors = FALSE
+)
+for (coin in colnames(log_returns)) {
+  
+  test <- adf.test(log_returns[, coin])
+  
+  adf_results <- rbind(
+    adf_results,
+    data.frame(
+      Cryptocurrency = coin,
+      ADF_Statistic = unname(test$statistic),
+      P_Value = test$p.value,
+      Stationary = ifelse(test$p.value < 0.05, "Yes", "No")
+    )
+  )
+}
+write.csv(
+    adf_results,
+    "outputs/tables/adf_results.csv",
+    row.names = FALSE )
 
+library(FinTS)
+arch_results <- data.frame(
+  Cryptocurrency = character(),
+  LM_Statistic = numeric(),
+  P_Value = numeric(),
+  ARCH_Effect = character(),
+  stringsAsFactors = FALSE
+)
+for (coin in colnames(log_returns)) {
+  
+  mean_model <- lm(log_returns[, coin] ~ 1)
+  
+  test <- ArchTest(residuals(mean_model), lags = 12)
+  
+  arch_results <- rbind(
+    arch_results,
+    data.frame(
+      Cryptocurrency = coin,
+      LM_Statistic = unname(test$statistic),
+      P_Value = test$p.value,
+      ARCH_Effect = ifelse(test$p.value < 0.05, "Yes", "No")
+    )
+  )
+}
+write.csv(
+  arch_results,
+  "outputs/tables/arch_test_results.csv",
+  row.names = FALSE
+)
 
 
 
