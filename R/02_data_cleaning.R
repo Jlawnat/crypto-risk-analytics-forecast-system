b3bddca387f72e3a868b33812d682a16bb4325f7
Elliot Yang <ramsyhahaha@gmail.com>
2026-06-27 20:10:42 +1000

    log return plot update

--- R/02_data_cleaning.R
+++ R/02_data_cleaning.R
@@ -1,3 +1,7 @@
+library(quantmod)
+library(xts)
+library(tidyverse)
+
 crypto_prices <- merge(BTC, ETH, SOL, BNB)
 colnames(crypto_prices) <- c("BTC", "ETH", "SOL", "BNB")
 crypto_prices=na.omit(crypto_prices)
@@ -13,4 +17,32 @@ write_csv(
   "data/processed/cleaned_returns.csv"
 )
 
+library(ggplot2)
+library(tidyr)
 
+plot_data <- log_returns_df |>
+  pivot_longer(
+    cols = c(BTC, ETH, SOL, BNB),
+    names_to = "Cryptocurrency",
+    values_to = "Log_Return"
+  )
+
+p <- ggplot(plot_data,
+            aes(Date, Log_Return, colour = Cryptocurrency)) +
+  geom_line(linewidth = 0.4) +
+  labs(
+    title = "Daily Log Returns",
+    x = "Date",
+    y = "Log Return"
+  ) +
+  theme_minimal()
+
+dir.create("figures", showWarnings = FALSE)
+
+ggsave(
+  "figures/log_returns.png",
+  plot = p,
+  width = 10,
+  height = 5,
+  dpi = 300
+)
